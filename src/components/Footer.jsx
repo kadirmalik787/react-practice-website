5c21c4514173536632721e0bb66024332329a2c5
kadir <110757075+kadirmalik787@users.noreply.github.com>
2023-01-25 14:02:57 +0530

    change app name

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+     <footer>
+        <div>
+            <h1>TeckyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+
+            <h5>Follow us</h5>
+            <div>
+            <a href="https://youtube.com" target={"blank"} >Youtube</a>
+            <a href="https://instagram.com/kadir_malik2217" target={"blank"} >Instagram</a>
+            <a href="https://google.com" target={"blank"} >Google</a></div>
+        </div>
+     </footer>
+  )
+}
+
+export default Footer
